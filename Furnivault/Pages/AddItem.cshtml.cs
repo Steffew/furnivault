f35376e9ceb7b15939040bc339bfce75f8fcabbd
Steffew <xgenyofficial@gmail.com>
2023-11-09 15:48:45 +0100

    Add form tag

--- Furnivault/Pages/AddItem.cshtml.cs
+++ Furnivault/Pages/AddItem.cshtml.cs
@@ -3,10 +3,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Furnivault.Pages
 {
+    public class AddItemViewModel
+    {
+        public string ItemIdentifier { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+    }
+
     public class AddItemModel : PageModel
     {
+        [BindProperty]
+        public AddItemViewModel Item { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+
         }
     }
 }

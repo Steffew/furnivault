1fc2aedf54e2d84cc664d095c446a69345fcf2ba
Stew <xgenyofficial@gmail.com>
2023-11-10 03:28:01 +0100

    Add IItemRepository

---
+++ Furnivault.Data/Repositories/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using Furnivault.Data.DTOs;
+
+namespace Furnivault.Data.Repositories
+{
+    public interface IItemRepository
+    {
+        ItemDTO GetItemById(int itemId);
+        IEnumerable<ItemDTO> GetAllItems();
+        void AddItem(ItemDTO item);
+        void UpdateItem(ItemDTO item);
+        void DeleteItem(int itemId);
+    }
+}

f68f525d84340a98127acb4b0d023e459a39d470
Stew <xgenyofficial@gmail.com>
2023-11-10 23:18:00 +0100

    Add GetItemById method

--- Furnivault.Data/Repositories/ItemRepository.cs
+++ Furnivault.Data/Repositories/ItemRepository.cs
@@ -40,9 +40,33 @@ namespace Furnivault.Data.Repositories
             }
             return items;
         }
-
         public ItemDTO GetItemById(int itemId)
         {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT ItemId, Name, Identifier, Favorite, Description, Image FROM Items WHERE ItemId = @ItemId";
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@ItemId", itemId);
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new ItemDTO
+                            {
+                                ItemId = reader.GetInt32(reader.GetOrdinal("ItemId")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Identifier = reader.IsDBNull(reader.GetOrdinal("Identifier")) ? null : reader.GetString(reader.GetOrdinal("Identifier")),
+                                Favorite = reader.GetBoolean(reader.GetOrdinal("Favorite")),
+                                Description = reader.IsDBNull(reader.GetOrdinal("Description")) ? null : reader.GetString(reader.GetOrdinal("Description")),
+                                Image = reader.IsDBNull(reader.GetOrdinal("Image")) ? null : (byte[])reader["Image"]
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
         }
 
         public void AddItem(ItemDTO item)

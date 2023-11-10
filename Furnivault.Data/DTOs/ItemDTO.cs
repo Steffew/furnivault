fc98574ab49bbfea4a7156bb58bee072934a09e9
Stew <xgenyofficial@gmail.com>
2023-11-10 02:13:12 +0100

    Add ItemDTO

---
+++ Furnivault.Data/DTOs/ItemDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Furnivault.Data.DTOs
+{
+    public class ItemDTO
+    {
+        public string Name { get; set; }
+        public int ItemId { get; set; }
+        public string Identifier { get; set; }
+        public bool Favorite { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+    }
+}

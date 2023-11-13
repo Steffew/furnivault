0d400b9fbcca5103161cc8e5de1f4408b2874b42
Steffew <steffewm@gmail.com>
2023-11-13 13:50:34 +0100

    Update Index model

--- Furnivault/Pages/AddItem.cshtml.cs
+++ Furnivault/Pages/AddItem.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Furnivault.Data.DTOs;
+using Furnivault.Data.Repositories;
 
 namespace Furnivault.Pages
 {
@@ -12,16 +14,37 @@ namespace Furnivault.Pages
 
     public class AddItemModel : PageModel
     {
+        private readonly IItemRepository _itemRepository;
+
         [BindProperty]
         public AddItemViewModel Item { get; set; }
+
+        public AddItemModel(IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
 
         public void OnGet()
         {
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
 
+            var itemDto = new ItemDTO
+            {
+                Name = Item.ItemName,
+                Identifier = Item.ItemIdentifier,
+                Description = Item.ItemDescription,
+            };
+
+            _itemRepository.AddItem(itemDto);
+
+            return RedirectToPage("./Index");
         }
     }
 }

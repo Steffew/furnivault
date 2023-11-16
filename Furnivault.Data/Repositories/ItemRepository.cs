24ae1812e89bee549adef60d58b0eb3c2c25bf48
Steffew <xgenyofficial@gmail.com>
2023-11-16 15:41:45 +0100

    Make line more concise

--- Furnivault.Data/Repositories/ItemRepository.cs
+++ Furnivault.Data/Repositories/ItemRepository.cs
@@ -15,26 +15,26 @@ namespace Furnivault.Data.Repositories
         public IEnumerable<ItemDTO> GetAllItems()
         {
             var items = new List<ItemDTO>();
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                string sql = "SELECT ItemId, Name, Identifier, Favorite, Description, Image FROM Items";
-                using var command = new SqlCommand(sql, connection);
-                connection.Open();
-                using var reader = command.ExecuteReader();
+            using SqlConnection connection = new(_connectionString);
 
-                while (reader.Read())
+            string sql = "SELECT ItemId, Name, Identifier, Favorite, Description, Image FROM Items";
+            using var command = new SqlCommand(sql, connection);
+            connection.Open();
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                items.Add(new ItemDTO
                 {
-                    items.Add(new ItemDTO
-                    {
-                        ItemId = reader.GetInt32(reader.GetOrdinal("ItemId")),
-                        Name = reader.GetString(reader.GetOrdinal("Name")),
-                        Identifier = reader.IsDBNull(reader.GetOrdinal("Identifier")) ? null : reader.GetString(reader.GetOrdinal("Identifier")),
-                        Favorite = reader.GetBoolean(reader.GetOrdinal("Favorite")),
-                        Description = reader.IsDBNull(reader.GetOrdinal("Description")) ? null : reader.GetString(reader.GetOrdinal("Description")),
-                        Image = reader.IsDBNull(reader.GetOrdinal("Image")) ? null : (byte[])reader["Image"]
-                    });
-                }
+                    ItemId = reader.GetInt32(reader.GetOrdinal("ItemId")),
+                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                    Identifier = reader.IsDBNull(reader.GetOrdinal("Identifier")) ? null : reader.GetString(reader.GetOrdinal("Identifier")),
+                    Favorite = reader.GetBoolean(reader.GetOrdinal("Favorite")),
+                    Description = reader.IsDBNull(reader.GetOrdinal("Description")) ? null : reader.GetString(reader.GetOrdinal("Description")),
+                    Image = reader.IsDBNull(reader.GetOrdinal("Image")) ? null : (byte[])reader["Image"]
+                });
             }
+
             return items;
         }
 

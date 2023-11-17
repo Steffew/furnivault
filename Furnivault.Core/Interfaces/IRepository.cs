76b9c0a7262651bd7fb1d78c49e66d446e414253
Steffew <xgenyofficial@gmail.com>
2023-11-17 14:16:55 +0100

    Add IRepository

---
+++ Furnivault.Core/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Furnivault.Core.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+    }
+}

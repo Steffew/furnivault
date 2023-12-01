bfa3bff74a0f041a1c8f95f0494a142171c8e9ba
Steffew <xgenyofficial@gmail.com>
2023-12-01 14:43:21 +0100

    Make Image nullable

--- Furnivault.Core/DTOs/ItemDTO.cs
+++ Furnivault.Core/DTOs/ItemDTO.cs
@@ -7,6 +7,6 @@
         public string Identifier { get; set; }
         public bool Favorite { get; set; }
         public string Description { get; set; }
-        public byte[] Image { get; set; }
+        public byte[]? Image { get; set; }
     }
 }

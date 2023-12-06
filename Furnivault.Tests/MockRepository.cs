18ec8edbb75b3670d91c2de0dfe6be6b24ea4842
Steffew <xgenyofficial@gmail.com>
2023-12-06 11:23:37 +0100

    Add unit tests

---
+++ Furnivault.Tests/MockRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Furnivault.Core.Entities;
+using Furnivault.Core.Interfaces;
+
+namespace Furnivault.Tests
+{
+    internal class MockRepository : IRepository<Item>
+    {
+        public string Name { get; set; }
+        public string Identifier { get; set; }
+        public string Description { get; set; }
+
+        public Item GetById(int itemId)
+        {
+            return new Item("Item", $"{itemId}", "Description", false);
+        }
+
+        public IEnumerable<Item> GetAll()
+        {
+            return new List<Item>();
+        }
+
+        public void Add(Item item)
+        {
+            new List<Item>().Add(item);
+        }
+
+        public void Update(Item item)
+        {
+            Name = item.Name;
+            Identifier = item.Identifier;
+            Description = item.Description;
+        }
+
+        public void Delete(int id)
+        {
+            List<Item> list = new List<Item>();
+            list.RemoveAt(id);
+        }
+    }
+}
---
+++ Furnivault.Tests/UnitTest1.cs
@@ -0,0 +1,53 @@
+namespace Furnivault.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Get_all_items()
+        {
+            MockRepository mockRepository = new MockRepository();
+            ItemService itemService = new ItemService(mockRepository);
+
+            IEnumerable<Item> items = itemService.GetAll();
+
+            Assert.IsType<List<Item>>(items);
+        }
+
+        [Fact]
+        public void Get_item_by_id()
+        {
+            MockRepository mockRepository = new MockRepository();
+            ItemService itemService = new ItemService(mockRepository);
+
+            Item item = itemService.GetById(1);
+
+            Assert.Equal("1", item.Identifier);
+        }
+
+        [Fact]
+        public void Add_item()
+        {
+            MockRepository mockRepository = new MockRepository();
+            ItemService itemService = new ItemService(mockRepository);
+
+            Item item = itemService.Add("Item", "1", "Description");
+
+            Assert.Equal("Item", item.Name);
+            Assert.Equal("1", item.Identifier);
+            Assert.Equal("Description", item.Description);
+        }
+
+        [Fact]
+        public void Update_item()
+        {
+            MockRepository mockRepository = new MockRepository();
+            ItemService itemService = new ItemService(mockRepository);
+
+            itemService.Update(1, "newItem", "newIdentifier", "newDescription");
+
+            Assert.Equal("newItem", mockRepository.Name);
+            Assert.Equal("newIdentifier", mockRepository.Identifier);
+            Assert.Equal("newDescription", mockRepository.Description);
+        }
+    }
+}

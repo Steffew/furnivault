4257f3b4764699062b318e31821e5a2ce84ff13b
Steffew <xgenyofficial@gmail.com>
2023-12-06 11:23:20 +0100

    Use ItemService instead of Repository

--- Furnivault/Pages/Index.cshtml.cs
+++ Furnivault/Pages/Index.cshtml.cs
@@ -4,16 +4,16 @@ using Furnivault.Core.Entities;
 
 public class IndexModel : PageModel
 {
-    private readonly IRepository<Item> _itemRepository;
     public List<Item> Items { get; private set; }
+    private readonly ItemService _itemService;
 
-    public IndexModel(IRepository<Item> itemRepository)
+    public IndexModel(IRepository<Item> repo)
     {
-        _itemRepository = itemRepository;
+        _itemService = new ItemService(repo);
     }
 
     public void OnGet()
     {
-        Items = _itemRepository.GetAll().ToList();
+        Items = _itemService.GetAll().ToList();
     }
 }

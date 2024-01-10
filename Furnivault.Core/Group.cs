e890bf69f6f240dea513046d5bdbc6155db78a4c
Steffew <xgenyofficial@gmail.com>
2024-01-10 15:19:58 +0100

    Add group entity

---
+++ Furnivault.Core/Group.cs
@@ -0,0 +1,29 @@
+﻿namespace Furnivault.Core.Entities
+{
+    public class Group
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public List<Item> Items { get; private set; }
+
+        public Group(string name)
+        {
+            Name = name;
+        }
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            Items.Remove(item);
+        }
+    }
+}
